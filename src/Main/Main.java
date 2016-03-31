81d04dc7b40f19f9633d37339441979016aec224
zzhadow <simon_00@edu.sollentuna.se>
2016-03-31 18:47:27 +0200

    Getting the scanner in

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,11 +1,48 @@
 package Main;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class Main {
 
-//printing text on the screen!
+
 	public static void main(String[] args) {
 		
-		System.out.println("yeah");
+		String verbFormLine;
+		
+		Collection<VerbForm> verbFormArray = new ArrayList<VerbForm>();
+		
+		try{
+		BufferedReader inFile = new BufferedReader(new FileReader("Verbs.txt"));
+		
+		while((verbFormLine = inFile.readLine()) !=null ){
+			
+			String[] verbFormParts = verbFormLine.split(";");
+			
+			VerbForm verbform = new VerbForm();
+			
+			verbform.verb = verbFormParts[0];
+			verbform.firstPerson = verbFormParts[1];
+			verbform.secondPerson = verbFormParts[2];
+			verbform.thirdPerson = verbFormParts[3];
+			verbform.fourthPerson = verbFormParts[4];
+			verbform.fifthPerson = verbFormParts[5];
+			verbform.sixthPerson = verbFormParts[6];
+			
+			verbFormArray.add(verbform);
+			
+		}
+		inFile.close();
+		
+		}catch(IOException e){
+			
+			System.err.println("Error " + e);
+		}
+		
+	
 	}
 
 }
---
+++ src/Main/VerbForm.java
@@ -0,0 +1,13 @@
+package Main;
+
+public class VerbForm {
+
+	public String verb;
+	public String firstPerson;
+	public String secondPerson;
+	public String thirdPerson;
+	public String fourthPerson;
+	public String fifthPerson;
+	public String sixthPerson;
+	
+}
